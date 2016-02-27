11f41f395d0a4fda18fb5e9aeb228e34a782a484
Timothy Knapik <knapikt@gmail.com>
2016-02-27 13:25:51 -0800

    Initial Commit

---
+++ PracticeC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  PracticeC
+//
+//  Created by Timothy Knapik on 2/27/16.
+//  Copyright © 2016 Tim. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
